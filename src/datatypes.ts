114aec955728489e19837179de8ecb0452c76a06
Orpheus Peng <stannolite@gmail.com>
2020-10-31 17:00:54 -0700

    Define Card data types according to the backend schema

---
+++ src/datatypes.ts
@@ -0,0 +1,40 @@
+export enum CardResourceType {
+  URL = 'URL',
+  TXT = 'TXT',
+  IMG = 'IMG',
+}
+
+export interface InformationCardResource {
+  resource: string;
+  resourceText?: string;
+  resourceType: CardResourceType;
+}
+
+export interface InformationCardData {
+  resourceCode: string;
+  department: string;
+  resources?: InformationCardResource[];
+  owner: string;
+  supportingText?: string;
+  authRequired: boolean;
+}
+
+export enum ForwardConditionType {
+  URL_MATCH = 'URL_MATCH',
+}
+
+export interface TutorialCardResource {
+  resource: string;
+  resourceType: CardResourceType;
+  forwardCondition: string;
+  forwardConditionType: ForwardConditionType;
+}
+
+export interface TutorialCardData {
+  resourceCode: string;
+  department: string;
+  resources?: TutorialCardResource[];
+  owner: string;
+  supportingText?: string;
+  authRequired: boolean;
+}
