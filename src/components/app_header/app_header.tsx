d3113db4ff3b0f669edd6710717a21fc3823b582
Orpheus Peng <stannolite@gmail.com>
2020-11-04 10:17:11 -0800

    Renamed Header to AppHeader

--- src/components/app_header/app_header.tsx
+++ src/components/app_header/app_header.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
-import './header.scss';
+import './app_header.scss';
 
 export interface HeaderProps {
   onToggleHeader: (isCollapsed: boolean) => void;
 }
 /** Chat Box component */
-export function Header({ onToggleHeader }: HeaderProps): JSX.Element {
+export function AppHeader({ onToggleHeader }: HeaderProps): JSX.Element {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const onClick = () => {
@@ -16,7 +16,7 @@ export function Header({ onToggleHeader }: HeaderProps): JSX.Element {
   };
 
   return (
-    <div className='header' onClick={onClick}>
+    <div className='app-header' onClick={onClick}>
       {'OHM'}
     </div>
   );
