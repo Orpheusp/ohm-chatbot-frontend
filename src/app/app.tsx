a0cb28d9ab094f527be742203ef8092ea5eb4ad3
Orpheus Peng <stannolite@gmail.com>
2020-11-04 10:17:57 -0800

    Change box-sizing model to border-box

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import {
+  userChatCard,
+  botChatCard,
+  botChatCard2,
+  urlInfoCardData,
+  urlInfoCardDataWithMultipleResources,
+} from 'src/card_data';
+import { AppHeader } from 'src/components/app_header/app_header';
+import { ChatPane } from 'src/components/chat_pane/chat_pane';
+import { ChatBox } from 'src/components/chat_box/chat_box';
+import { BaseCardData } from 'src/datatypes';
+
 export class App extends React.Component {
+  private messages: BaseCardData[] = [
+    botChatCard,
+    userChatCard,
+    botChatCard2,
+    urlInfoCardData,
+    urlInfoCardDataWithMultipleResources,
+  ];
+
   render(): JSX.Element {
-    return <div> OHM </div>;
+    return (
+      <div className={'app'}>
+        <AppHeader onToggleHeader={() => {}} />
+        <ChatPane chats={this.messages} />
+        <ChatBox onSubmit={() => {}} />
+      </div>
+    );
   }
 }
