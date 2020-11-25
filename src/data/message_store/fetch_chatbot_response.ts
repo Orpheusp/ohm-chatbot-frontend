79f7e4ec6efa957fff889786991c5c644752c566
Orpheus Peng <stannolite@gmail.com>
2020-11-24 23:15:49 -0800

    Modify dev and prod chat api endpoint

--- src/data/message_store/fetch_chatbot_response.ts
+++ src/data/message_store/fetch_chatbot_response.ts
@@ -1,10 +1,18 @@
 import { BaseCardData } from 'src/datatypes';
 import { MessageStoreAction } from './message_store_action';
 
+/**
+ * For dogfood purposes, we assume a local backend server is being run for both
+ * dev and prod environment, until later on a proper remote backend server is
+ * deployed.
+ */
 export const CHATBOT_ENDPOINTS = {
+  // In a dev environment, the app is run on a local dev server. Due to CROS
+  // reasons, a local proxy is needed to redirect our requests and responses.
   development: 'http://localhost:8010/proxy/chats',
   test: 'http://localhost:8010/proxy/chats',
-  production: '',
+  // In a prod environment, the app is run in Chrome, and no proxy is needed.
+  production: 'http://127.0.0.1:5000/chats',
 };
 
 /** Fetch chatbot data from backend. */
