71c3f3771888aa10c226cfb862bad24cf7efd5f5
Orpheus Peng <stannolite@gmail.com>
2020-11-30 05:03:39 -0800

    Connect to background script

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import * as ReactDOM from 'react-dom';
 
 import { App } from './app/app';
 
+if (chrome?.runtime) {
+  chrome.runtime.connect();
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
